deff2ab2a1833f533f8fa78dff90775900c107f2
wasswaenockmale <enockmale97@gmail.com>
2023-02-17 09:30:07 +0300

    I have started a todolist application

---
+++ src/Components/App/index.js
@@ -0,0 +1,14 @@
+
+function App() {
+    return (
+      <div className="center-div">
+        <p>
+          Welcome to your daily schedule system.
+          Login to setup today's program.
+        </p>
+      </div>
+    );
+  }
+  
+  export default App;
+  
